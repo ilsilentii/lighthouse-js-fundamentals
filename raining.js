bbb63f55a20f8c07ff1b0d7df5388a2fa8bec6f0
ilsilentii <travis.vdm@outlook.com>
2017-08-27 22:21:53 +0000

    Adds conditionals exercises

---
+++ raining.js
@@ -0,0 +1,5 @@
+raining = false;
+
+if (!raining) {
+  console.log("Leave your umbrella at home!");
+}
---
+++ voting.js
@@ -0,0 +1,6 @@
+var isCitizen = true;
+var age = 25;
+
+if (isCitizen && age > 18) {
+  console.log("You are eligible to vote.");
+}
